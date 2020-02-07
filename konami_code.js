756eb2e12956a80a35eabb7bf98cb318767cc46f
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-07 23:05:57 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,25 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
-}
+unction init() {
+  // your code here	  //
+  var index = 0
+  document.body.addEventListener('keydown', onKeyDownHandler);
+
+  function onKeyDownHandler(e) {
+    const key = parseInt(e.detail || e.which)
+    console.log('key', key)
+    if (key === code[index]) {
+      index++
+
+      if (index === code.length) {
+        alert("YOU DID IT!")
+        index = 0
+      }
+    } else {
+      index = 0
+    }
+  }
+}	
+
+
